99baf47723d8a8a591a6e3dbd3065b8839206d42
Fatima Farman <YOUR_GITHUB_EMAIL_HERE>
2026-01-22 14:03:42 +0500

    Sandbox mode: disable snapshots and run models as tables

--- domains/people_analytics/models/marts/fct_hiring_funnel_incremental.sql
+++ domains/people_analytics/models/marts/fct_hiring_funnel_incremental.sql
@@ -1,23 +1,14 @@
-{{ config(
-    materialized='incremental',
-    unique_key='event_id',
-    on_schema_change='sync_all_columns'
-) }}
+{{ config(materialized='table') }}
 
 with src as (
     select
         cast(event_id as int64) as event_id,
         cast(candidate_id as int64) as candidate_id,
         cast(stage as string) as stage,
-        TIMESTAMP(event_ts) as event_ts
+        timestamp(event_ts) as event_ts
     from {{ ref('hiring_events_incremental_demo') }}
 )
 
 select *
 from src
-
-{% if is_incremental() %}
-where event_ts > (select TIMESTAMP(max(event_ts)) from {{ this }})
-{% endif %}
-
 
